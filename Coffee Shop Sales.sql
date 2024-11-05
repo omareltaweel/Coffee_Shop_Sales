12e3c9953694fb5bb5052a9a8a009af74356839d
omareltaweel <77691383+omareltaweel@users.noreply.github.com>
2024-11-05 03:20:19 +0200

    Add files via upload

---
+++ Coffee Shop Sales.sql
@@ -0,0 +1,29 @@
+SELECT * FROM coffee.coffee_shop_sales;
+DESCRIBE coffee.coffee_shop_sales ;
+
+SELECT COUNT(transaction_id) as Total_Orders
+FROM coffee.coffee_shop_sales
+WHERE  transaction_date = 2023-01-01 -- for month of (CM-May)
+
+SELECT 
+    day_of_month,
+    CASE 
+        WHEN total_sales > avg_sales THEN 'Above Average'
+        WHEN total_sales < avg_sales THEN 'Below Average'
+        ELSE 'Average'
+    END AS sales_status,
+    total_sales
+FROM (
+    SELECT 
+        DAY(transaction_date) AS day_of_month,
+        SUM(unit_price * transaction_qty) AS total_sales,
+        AVG(SUM(unit_price * transaction_qty)) OVER () AS avg_sales
+    FROM 
+        coffee.coffee_shop_sales
+    WHERE 
+        MONTH(transaction_date) = 5  -- Filter for May
+    GROUP BY 
+        DAY(transaction_date)
+) AS sales_data
+ORDER BY 
+    day_of_month;
